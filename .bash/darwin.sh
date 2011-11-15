330fc45f51598b7f440f0df95ee494549cee3358
Fuji, Goro <fuji.goro@dena.jp>
2011-11-15 11:18:57 +0900

    Enable bash_completion in MacOSX

--- .bash/darwin.sh
+++ .bash/darwin.sh
@@ -1,6 +1,10 @@
 #!bash
 # MacOSX-specific resources
 
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+    . `brew --prefix`/etc/bash_completion
+fi
+
 ANDROID_SDK="$HOME/dev/android-sdk-mac_x86"
 ANDROID_NDK="$HOME/android/ndk-r4b"
 export PATH="$ANDROID_NDK:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools:$PATH"
@@ -11,7 +15,5 @@ alias vim='"$EDITOR" "$@"'
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
-#export LC_ALL=C
 export LANG=ja_JP.UTF-8
 
-export PATH="$HOME/flex_sdk_4.5.1/bin:$PATH"
