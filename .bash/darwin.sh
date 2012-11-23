8c9ab726d076c339f337b8bec1c44f23e45955f3
Fuji, Goro <fuji.goro@dena.jp>
2012-11-24 00:18:28 +0900

    mac setting

--- .bash/darwin.sh
+++ .bash/darwin.sh
@@ -1,16 +1,21 @@
 #!bash
 # MacOSX-specific resources
 
-BREW_PREFIX=`brew --prefix`
-if [ -f "$BREW_PREFIX/etc/bash_completion" ]; then
-    . "$BREW_PREFIX/etc/bash_completion"
+if which brew > /dev/null ; then
+
+	BREW_PREFIX=`brew --prefix`
+	if [ -f "$BREW_PREFIX/etc/bash_completion" ]; then
+		. "$BREW_PREFIX/etc/bash_completion"
+	fi
+
+	export PATH="/Applications/dart/sdk/bin:$PATH"
+	export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+	export MANPATH="$(brew --prefix coreutils)/libexec/gnuman:$MANPATH"
+
+	export ANDROID_SDK=`brew --prefix android-sdk`
+	export ANDROID_NDK=`brew --prefix android-ndk`
+
 fi
-
-export PATH="/Applications/dart/sdk/bin:$PATH"
-export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
-
-ANDROID_SDK=`brew --prefix android-sdk`
-ANDROID_NDK=`brew --prefix android-ndk`
 
 export PATH="/Applications/MacVim.app/Contents/MacOS:$PATH"
 
